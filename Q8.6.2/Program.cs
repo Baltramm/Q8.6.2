3c3411a1a378b345d3d5d2a120ee14ff8ee39e18
Nikita Bochkovskiy <nikita.bochkovsky@yandex.ru>
2022-05-02 22:17:38 +0600

    Добавьте файлы проекта.

---
+++ Q8.6.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+namespace Q8._6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"F://Steam";
+            if (!File.Exists(filePath))
+            {
+                try
+                {
+                    var directory = new DirectoryInfo(filePath);
+                    Console.WriteLine("Размер в байтах:{0}",DirectorySize.DirSize(directory));
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+    public static class DirectorySize
+    {
+        public static long DirSize (DirectoryInfo d)
+        {
+            long size = 0;
+            FileInfo[] files = d.GetFiles();
+            foreach(FileInfo fi in files)
+            {
+                size += fi.Length;
+            }
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += DirSize(di);
+
+            }
+            return size;
+        }
+    }
+}
